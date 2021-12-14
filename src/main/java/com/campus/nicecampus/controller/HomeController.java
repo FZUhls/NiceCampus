47b505e3e57ca287a7213f3d278fca109ba800c5
FZUhls <314433307@qq.com>
2021-12-14 17:34:18 +0800

    12/14 update 2

--- src/main/java/com/campus/nicecampus/controller/HomeController.java
+++ src/main/java/com/campus/nicecampus/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.campus.nicecampus.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController extends BaseController {
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("user",getUser());
         return "index";
     }
 }
